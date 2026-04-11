d2f0611bab430694f50a1abc7912d7aa0b68e362
KAWKAW <zahir@gmail.com>
2026-04-11 14:08:24 +0100

    creation de function registre avce form request pour validation  à controller auth controller

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -1,15 +1,35 @@
+
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
+public function register(RegisterRequest $request)
+{
+
+    $role = User::count() === 0 ? 'Admin' : 'Donor';
 
 
+    $data = $request->validated(); 
+    $data['password'] = Hash::make($request->password);
+    $data['role'] = $role;
 
+    // Logique spécifique au rôle Donor
+    if ($role === 'Donor') {
+        $data['blood_group'] = $request->blood_group ?? 'Unknown';
+        $data['status_availabality'] = true;
+        $data['is_verified'] = false;
+    } else {
+        $data['is_verified'] = null;
+    }
 
+    $user = User::create($data);
 
-
+    return response()->json([
+        'message' => "Vous êtes inscrit avec succès",
+        'user' => $user
+    ]);
+}
 }
