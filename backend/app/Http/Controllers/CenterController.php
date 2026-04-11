f5a483f4cf6ba46a06eb68a44542a84a8c0c94e6
KAWKAW <zahir@gmail.com>
2026-04-11 18:22:38 +0100

    creation de function voirrequest de hopital

--- backend/app/Http/Controllers/CenterController.php
+++ backend/app/Http/Controllers/CenterController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BloodRequest;
+use App\Models\BloodStock;
+use App\Models\Notification;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class CenterController extends Controller
 {
-    //
+    public function  voirRequest(){
+        $user = auth()->user();
+
+
+    if (!$user || !$user->centre) {
+        return response()->json([
+            'message' => 'Erreur : Aucun centre associé à cet utilisateur.'
+        ], 404);
+    }
+
+    $center = $user->centre;
+
+
+    $requests = BloodRequest::with('hopital')
+        ->where('center_id', $center->id)
+        ->orderBy('priority', 'desc')
+        ->latest()
+        ->get();
+
+    return response()->json($requests);
+
+    }
+    
+
 }
