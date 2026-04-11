4a470a4a89c7403ed5f0e308f5f10e0db128b37b
KAWKAW <zahir@gmail.com>
2026-04-11 01:10:05 +0100

    creation  relation de user

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,13 +9,24 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+
 class User extends Authenticatable
 {
+    protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'phone', 
+    'role',
+    'city',
+    'blood_group',
+    'is_verified',
+    'status_availabality',
+];
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiTokens;
 
     /**
      * Get the attributes that should be cast.
@@ -28,5 +39,31 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
+public function centre(){
+    return $this->hasOne(Centre::class);
+}
+public function hopital(){
+    return $this->hasOne(Hopital::class);
+}
+public function reports(){
+        $this->hasMany(Report::class);
+}
+public function donations(){
+        $this->hasMany(Donation::class);
+}
 }
